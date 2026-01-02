dde3cd8dd57cff3400785eacb9dc80170edda917
Manik Roy <pixnum@gmail.com>
2026-01-02 17:19:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE="rivanoy-v1";
-const ASSETS=["./","./index.html","./manifest.json"];
-
+const CACHE="rivanoy-v4";
 self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      "./","./index.html","./manifest.json"
+    ]))
+  );
 });
-
 self.addEventListener("activate",e=>{
   e.waitUntil(
     caches.keys().then(k=>Promise.all(k.map(x=>x!==CACHE&&caches.delete(x))))
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(
-    caches.match(e.request).then(r=>
-      r||fetch(e.request).then(n=>{
-        caches.open(CACHE).then(c=>c.put(e.request,n.clone()));
-        return n;
-      }).catch(()=>caches.match("./"))
-    )
-  );
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
